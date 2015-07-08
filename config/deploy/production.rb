1d57b16120cd49e2fdeb8052e441d667f063ea98
t00l <moisesdonoso@gmail.com>
2015-07-07 21:33:32 -0300

    fix gem pg

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,5 +3,5 @@ role :web, %w{deploy@45.55.91.184}
 role :db,  %w{deploy@45.55.91.184}
 
 
-server '45.55.91.184', user: 'deploy', roles: %w{web app}, my_property: :my_value
+server '45.55.91.184', user: 'deploy', roles: %w{web app}
 set :stage, :production
